89598c0875d2e79eb345b9dda0661c3dcd87cac4
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-05-21 16:29:21 +0300

    fix

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -1,2 +1,27 @@
 #set env vars
 set -o allexport; source .env; set +o allexport;
+
+
+AUTHENTIK_SECRET_KEY=${AUTHENTIK_SECRET_KEY:=`openssl rand -base64 36`}
+
+cat << EOT >> ./.env
+
+AUTHENTIK_SECRET_KEY=${AUTHENTIK_SECRET_KEY}
+EOT
+
+cat <<EOT > ./servers.json
+{
+    "Servers": {
+        "1": {
+            "Name": "local",
+            "Group": "Servers",
+            "Host": "172.17.0.1",
+            "Port": 42501,
+            "MaintenanceDB": "postgres",
+            "SSLMode": "prefer",
+            "Username": "postgres",
+            "PassFile": "/pgpass"
+        }
+    }
+}
+EOT
