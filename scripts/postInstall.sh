15988ef58635bc24d6036dfa3fd7c4545724a0f0
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-05-22 12:32:30 +0300

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -3,5 +3,21 @@ set -o allexport; source .env; set +o allexport;
 
 #wait until the server is ready
 echo "Waiting for software to be ready ..."
-sleep 30s;
+
+sleep 60s;
+
+    docker-compose exec -T wk-oidc-server bash -c "make init"
+	docker-compose exec -T wk-oidc-server bash -c "python manage.py loaddata oidc-server-outline-client"
+
+
+
+
+    docker-compose exec -T wk-oidc-server sh -c "python manage.py createsuperuser --noinput --username=admin --email=$ADMIN_EMAIL"
+    docker-compose exec -T wk-oidc-server sh -c "python manage.py shell <<EOF
+from django.contrib.auth.models import User
+
+user = User.objects.get(username='admin')
+user.set_password('$ADMIN_PASSWORD')
+user.save()
+EOF"
 
--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -1,27 +1,66 @@
 #set env vars
 set -o allexport; source .env; set +o allexport;
 
+# apt install jq -y
 
-AUTHENTIK_SECRET_KEY=${AUTHENTIK_SECRET_KEY:=`openssl rand -base64 36`}
+chmod +x ./scripts/*.sh
+mkdir -p ./config/uc/fixtures
+chown -R 1000:1000 ./config/uc/fixtures
+
+if [ -e "./config/uc/fixtures/oidc-server-grist-client.json" ]; then
+   exit 0;
+fi
+
+OIDC_CLIENT_SECRET=${MINIO_SECRET_KEY:-`openssl rand -hex 28`}
+GRIST_SECRET_KEY=${GRIST_SECRET_KEY:-`openssl rand -hex 32`}
+
 
 cat << EOT >> ./.env
 
-AUTHENTIK_SECRET_KEY=${AUTHENTIK_SECRET_KEY}
+OIDC_CLIENT_SECRET=${OIDC_CLIENT_SECRET}
+DEBUG=0
+LANGUAGE_CODE=en-us
+TIME_ZONE=UTC
+FORCE_SCRIPT_NAME=/uc
+SECRET_KEY=${GRIST_SECRET_KEY}
+
 EOT
 
-cat <<EOT > ./servers.json
-{
-    "Servers": {
-        "1": {
-            "Name": "local",
-            "Group": "Servers",
-            "Host": "172.17.0.1",
-            "Port": 22501,
-            "MaintenanceDB": "postgres",
-            "SSLMode": "prefer",
-            "Username": "postgres",
-            "PassFile": "/pgpass"
-        }
+
+cat << EOT >> ./config/uc/fixtures/oidc-server-grist-client.json
+
+[
+    {
+     "model": "oidc_provider.client",
+     "pk": 1,
+     "fields": {
+      "name": "outline",
+      "owner": null,
+      "client_type": "confidential",
+      "client_id": "050984",
+      "client_secret": "$OIDC_CLIENT_SECRET",
+      "jwt_alg": "RS256",
+      "date_created": "2022-02-15",
+      "website_url": "",
+      "terms_url": "",
+      "contact_email": "",
+      "logo": "",
+      "reuse_consent": true,
+      "require_consent": true,
+      "_redirect_uris": "https://$DOMAIN/auth/oidc.callback",
+      "_post_logout_redirect_uris": "",
+      "_scope": "",
+      "response_types": [
+       1,
+       2,
+       3,
+       4,
+       5,
+       6
+      ]
+     }
     }
-}
+    ]
+
+
 EOT
